916d3ccd0c2ef5eae6079e3a9ad34fd6a563190f
servotronik <servotronik@mail.ru>
2020-06-24 18:34:12 +0300

    WR-1 Spring init

---
+++ src/main/java/ru/jigrinyuk/WR_Spring/WrSpringApplication.java
@@ -0,0 +1,13 @@
+package ru.jigrinyuk.WR_Spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WrSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WrSpringApplication.class, args);
+	}
+
+}
